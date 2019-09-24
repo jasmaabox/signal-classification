dfb5b602bc255b2f9a632bfc3901c011b4c9f4d0
jasmaa <jasonmaa3955@gmail.com>
2019-09-24 09:28:51 -0400

    Work on scraper

---
+++ data_scraper/scrape.py
@@ -0,0 +1,12 @@
+import youtube_dl
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+        'preferredquality': '192',
+    }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=oHg5SJYRHA0'])
